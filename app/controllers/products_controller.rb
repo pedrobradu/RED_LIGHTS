0621835ed4fa0930cb7157f871b17f3abf525438
Nathalia Pinho <nathaliapinho.d@gmail.com>
2022-08-23 14:59:32 -0300

    metodo show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,17 +1,9 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: %i[show]
-
   def index
     @products = Product.all
   end
 
   def show
-    @product = Product.new
-  end
-
-  private
-
-  def set_product
     @product = Product.find(params[:id])
   end
 end
