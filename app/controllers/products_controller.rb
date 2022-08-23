6b4547770553883e1b40140de92cc91e97e99cd5
Caroline Brum <carolinefbrum@gmail.com>
2022-08-23 15:07:01 -0300

    methods new and create add

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,24 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :category, :description)
+  end
 end
