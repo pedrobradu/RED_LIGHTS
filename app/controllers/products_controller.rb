2ff3988bed4693a4a2656604169c818a7a85f43e
Nathalia Pinho <nathaliapinho.d@gmail.com>
2022-08-23 14:09:28 -0300

    primeiras mudancas

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,13 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: %i[show]
+
+  def show
+    @product = Product.new
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
 end
