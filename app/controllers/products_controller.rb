824049aa9aeebac55982329165fdd4131761e117
PEDRO DUARTE <pedrobradu@gmail.com>
2022-08-23 17:27:31 -0300

    show fora de private

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -29,19 +29,14 @@ class ProductsController < ApplicationController
     redirect_to product_path(@product)
   end
 
-  private
-
-  def product_params
-    params.require(:product).permit(:name, :price, :category, :description)
-  end
-
   def show
     @product = Product.find(params[:id])
   end
 
   private
 
-  # def product_params
-  #   params.require(:product).permit(:name, :image_url)
-  # end
+  def product_params
+    params.require(:product).permit(:name, :price, :category, :description)
+  end
+
 end
