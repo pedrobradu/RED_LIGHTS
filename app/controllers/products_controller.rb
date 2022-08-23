f09aff0433a9d7a55e5338dd1070d5cde4c547b7
Nathalia Pinho <nathaliapinho.d@gmail.com>
2022-08-23 16:54:12 -0300

    metodo e view de SHOW

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,10 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  private
+
+  # def product_params
+  #   params.require(:product).permit(:name, :image_url)
+  # end
 end
