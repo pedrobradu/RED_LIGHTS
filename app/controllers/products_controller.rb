7cbe6de728a9e9e3772cdee4463f349b54919fbd
Caroline Brum <carolinefbrum@gmail.com>
2022-08-23 17:30:44 -0300

    .

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -38,10 +38,4 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
-
-  private
-
-  # def product_params
-  #   params.require(:product).permit(:name, :image_url)
-  # end
 end
