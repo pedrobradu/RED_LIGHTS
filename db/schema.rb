2fc7d1df6c65f4a2e6c4577ced7d94f362f85125
PEDRO DUARTE <pedrobradu@gmail.com>
2022-08-24 13:57:38 -0300

    campo total price removido do model order

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_22_204528) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_24_165311) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "orders", force: :cascade do |t|
     t.integer "quantity"
-    t.integer "total_price"
     t.bigint "user_id", null: false
     t.bigint "product_id", null: false
     t.datetime "created_at", null: false
