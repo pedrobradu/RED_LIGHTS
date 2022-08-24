6155558a31ff6ea54cbfc05f76aeb4334b448650
Nathalia Pinho <nathaliapinho.d@gmail.com>
2022-08-24 11:49:08 -0300

    orders show button

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,32 +1,31 @@
 class OrdersController < ApplicationController
-    
-    def new
-        @product = Product.find(params[:product_id])
-        @order = Order.new
-    end
+  def new
+    @product = Product.find(params[:product_id])
+    @order = Order.new
+  end
 
-    def create
-        # @user = User.find(:id)
-        @product = Product.find(params[:product_id])
-        @order = Order.new(order_params)
-        @order.user = current_user
-        @order.product = @product
-        @order.save
-        if @order.save
-            redirect_to product_order_path(@order)
-        else
-            render :new, status: :unprocessable_entity
-        end
+  def create
+    # @user = User.find(:id)
+    @product = Product.find(params[:product_id])
+    @order = Order.new(order_params)
+    @order.user = current_user
+    @order.product = @product
+    @order.save
+    if @order.save
+      redirect_to product_order_path(@order)
+    else
+      render :new, status: :unprocessable_entity
     end
-    
-    def show
-        @product = Product.find(params[:product_id])
-        @order = Order.find(params[:id])
-    end
+  end
 
-    private
-    
-    def order_params
-        params.require(:order).permit(:quantity, :total_price)
-    end
+  def show
+    @product = Product.find(params[:product_id])
+    @order = Order.find(params[:id])
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:quantity, :total_price)
+  end
 end
